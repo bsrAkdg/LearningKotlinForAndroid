b8eec15b54015c0bcbe7b667696a549dd4b66b92
bsrAkdg <busraaakdag@gmail.com>
2019-01-13 15:35:09 +0300

    added extension class

---
+++ Notepad/app/src/main/java/notepadkotlin/bsrakdg/com/notepad/util/_LayoutInflater.kt
@@ -0,0 +1,7 @@
+package notepadkotlin.bsrakdg.com.notepad.util
+
+import android.content.Context
+import android.view.LayoutInflater
+
+val Context.layoutInflater: LayoutInflater
+    get() = LayoutInflater.from(this)
