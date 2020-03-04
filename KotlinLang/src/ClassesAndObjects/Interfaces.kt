4eb92f0324aa3ddc1f09686c15fb9f8469bb8dea
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-03-04 11:28:26 +0300

    Interfaces completed

--- KotlinLang/src/ClassesAndObjects/Interfaces.kt
+++ KotlinLang/src/ClassesAndObjects/Interfaces.kt
@@ -20,6 +20,11 @@ fun main() {
 
     // TODO Interfaces Inheritance
     interfacesInheritance()
+
+    println("\n****************************\n")
+
+    // TODO Resolving overriding conflicts
+    resolvingOverridingConflicts()
 }
 
 interface State {
@@ -81,6 +86,81 @@ fun propertiesInInterfaces() {
     fileState.logDefaultState()
 }
 
-fun interfacesInheritance() {
+interface Name {
+    val name: String
+}
 
+interface Person : Name {
+    val firstName: String
+    val lastName: String
+
+    override val name: String
+        get() = "$firstName $lastName"
+}
+
+fun interfacesInheritance() {
+    // An interface can derive from other interfaces and thus both provide implementations for their members and
+    // declare new functions and properties. Quite naturally, classes implementing such an interface are only
+    // required to define the missing implementations:
+
+    // Look at Name-Person interfaces and Employee class, Employee class must implement members of Person,
+    // but must not implement members of Name, because Person override name member,
+    // if Person doesn't implement name member, Employee must implement it.
+    data class Employee(override val firstName: String, override val lastName: String, val id: Int) : Person
+
+    val employee = Employee("Ali", "Veli", 1)
+    println(employee)
+}
+
+interface A {
+    fun foo() {
+        println("A foo")
+    }
+
+    fun bar()
+}
+
+interface B {
+    fun foo() {
+        println("B foo")
+    }
+
+    fun bar() {
+        println("B bar")
+    }
+}
+
+class C : A {
+    override fun bar() {
+        println("C override A bar")
+    }
+}
+
+class D : A, B {
+
+    override fun foo() {
+        println("D override foo")
+        super<A>.foo() // supertype list
+        super<B>.foo() // supertype list
+    }
+
+    override fun bar() {
+        println("D override bar")
+        super.bar()
+    }
+}
+
+fun resolvingOverridingConflicts() {
+    // When we declare many types in our supertype list, it may appear that we inherit more than one implementation of
+    // the same method.
+
+    println("C created : \n")
+    val c = C()
+    c.foo()
+    c.bar()
+
+    println("\nD created : \n")
+    val d = D()
+    d.foo()
+    d.bar()
 }
